120f1da107dc98a95c4cac21606efff41d6d9997
Sharwin Bobde <bsharwin@gmail.com>
2021-01-25 15:47:02 +0100

    save subscribed users

--- build.sbt
+++ build.sbt
@@ -25,3 +25,18 @@ libraryDependencies ++= Seq(
   "io.circe" %% "circe-generic",
   "io.circe" %% "circe-parser"
 ).map(_ % circeVersion)
+
+libraryDependencies  ++= Seq(
+  // Last stable release
+  "org.scalanlp" %% "breeze" % "1.1",
+
+  // Native libraries are not included by default. add this if you want them
+  // Native libraries greatly improve performance, but increase jar sizes.
+  // It also packages various blas implementations, which have licenses that may or may not
+  // be compatible with the Apache License. No GPL code, as best I know.
+//  "org.scalanlp" %% "breeze-natives" % "1.1",
+
+  // The visualization library is distributed separately as well.
+  // It depends on LGPL code
+  "org.scalanlp" %% "breeze-viz" % "1.1"
+)
--- src/main/scala/ListeningDistributions.scala
+++ src/main/scala/ListeningDistributions.scala
@@ -8,7 +8,12 @@ import utils.PercentileApprox._
 import document_schemas.UserToRecordingOrArtistRelation
 import ArangoDBHandler._
 import org.apache.spark.storage.StorageLevel
-import io.circe._, io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.types.{BooleanType, IntegerType, LongType}
 
 import java.io.{BufferedWriter, File, FileWriter}
 import scala.util.parsing.json.JSONObject
@@ -34,7 +39,6 @@ object ListeningDistributions {
 
     val sc = spark.sparkContext
     val user_rec_interactions = getUserToRecordingEdges(sc, spark)
-    user_rec_interactions.printSchema()
 
     //    from 2006 to 2018
     //    (2006 until 2007).foreach(i => {
@@ -43,34 +47,43 @@ object ListeningDistributions {
     //    })
 
     //  Look at user's total listens every year
-    //    user_rec_interactions
-    //      .select(col("_from").alias("user"), col("years.*"))
-    //      .groupBy("user")
-    //      .agg(
-    //        sum("yr_2005"),
-    //        sum("yr_2006"),
-    //        sum("yr_2007"),
-    //        sum("yr_2008"),
-    //        sum("yr_2009"),
-    //        sum("yr_2010"),
-    //        sum("yr_2011"),
-    //        sum("yr_2012"),
-    //        sum("yr_2013"),
-    //        sum("yr_2014"),
-    //      )
-    //      .coalesce(1)
-    //      .write
-    //      .mode(SaveMode.Overwrite)
-    //      .option("header", "true")
-    //      .csv(out_dir + "user_listens_per_year.csv")
+    //            user_rec_interactions
+    //              .select(col("_from"), col("years.*"))
+    //              .withColumn("users", substring(col("_from"), 7, 36))
+    //              .groupBy("users")
+    //              .agg(
+    //                sum("yr_2005"),
+    //                sum("yr_2006"),
+    //                sum("yr_2007"),
+    //                sum("yr_2008"),
+    //                sum("yr_2009"),
+    //                sum("yr_2010"),
+    //                sum("yr_2011"),
+    //                sum("yr_2012"),
+    //                sum("yr_2013"),
+    //                sum("yr_2014"),
+    //              )
+    //              .coalesce(1)
+    //              .write
+    //              .mode(SaveMode.Overwrite)
+    //              .option("header", "true")
+    //              .csv(out_dir + "user_listens_per_year.csv")
 
 
     // Look at percentile distributions of listens for every year
-    percentileUserListensByYear(user_rec_interactions)
+    //    percentileUserListensByYear(user_rec_interactions)
+
+
+    ////        get Subscribed uses by years
+    saveSubscribedUsersByYear(user_rec_interactions)
+
+    //         get percentile distributions of listens for every year normalised by the subscribers
+    subscriberNormalisedPercentileUserListensByYear(user_rec_interactions, spark)
 
 
     // Stop the underlying SparContext
     sc.stop
+    System.exit(0)
   }
 
   def percentileUserListensByYear(user_rec_interactions: Dataset[Row]): Unit = {
@@ -99,11 +112,85 @@ object ListeningDistributions {
           percentiles_to_extract, 0.0).asJson)
 
     })
-
-    val file = new File("out_data/user_rec_listen_percentiles_by_year.json")
-    val bw = new BufferedWriter(new FileWriter(file))
+    val bw = new BufferedWriter(new FileWriter(new File("out_data/user_rec_listen_percentiles_by_year.json")))
     bw.write(m.asJson.spaces2SortKeys)
     bw.close()
+  }
+
+  def subscriberNormalisedPercentileUserListensByYear(user_rec_interactions: Dataset[Row], spark: SparkSession): Unit = {
+    val percentiles_to_extract = Array(0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+
+    val df = user_rec_interactions
+      .select(col("_from"), col("years.*"))
+      .withColumn("user", substring(col("_from"), 7, 36))
+      .groupBy("user")
+      .agg(
+        sum(col("yr_2005")),
+        sum(col("yr_2006")),
+        sum(col("yr_2007")),
+        sum(col("yr_2008")),
+        sum(col("yr_2009")),
+        sum(col("yr_2010")),
+        sum(col("yr_2011")),
+        sum(col("yr_2012")),
+        sum(col("yr_2013")),
+        sum(col("yr_2014")),
+      )
+      .cache()
+    var m = Map[String, Json]()
+    (2005 until 2015).foreach(year => {
+      val year_subscribers_list = spark.read.option("header", "true")
+        .csv(out_dir + "year_" + year.toString + "_subscribers.csv")
+        .withColumnRenamed("users", "user")
+
+      val percentiles = df
+        .join(year_subscribers_list, Seq("user"), "inner")
+        .stat.approxQuantile("sum(yr_" + year.toString + ")",
+        percentiles_to_extract, 0.0)
+
+      m += (year.toString -> percentiles.asJson)
+
+    })
+
+
+    val bw = new BufferedWriter(new FileWriter(new File("out_data/normalised_user_rec_listen_percentiles_by_year.json")))
+    bw.write(m.asJson.spaces2SortKeys)
+    bw.close()
+  }
+
+  def saveSubscribedUsersByYear(user_rec_interactions: Dataset[Row]): Unit = {
+    val subscription_checker = udf((sum_for_user: Long) => {
+      sum_for_user > 0
+    }, BooleanType)
+
+    val df1 = user_rec_interactions
+      .select("_from", "years.*")
+      .groupBy("_from")
+      .agg(
+        sum("yr_2005"),
+        sum("yr_2006"),
+        sum("yr_2007"),
+        sum("yr_2008"),
+        sum("yr_2009"),
+        sum("yr_2010"),
+        sum("yr_2011"),
+        sum("yr_2012"),
+        sum("yr_2013"),
+        sum("yr_2014"),
+      )
+      .persist(StorageLevel.DISK_ONLY)
+
+    (2005 until 2015).foreach(year => {
+      df1.filter(subscription_checker(col("sum(yr_" + year.toString + ")")))
+        .withColumn("users", substring(col("_from"), 7, 36))
+        .select("users")
+        .persist(StorageLevel.DISK_ONLY)
+        .coalesce(1)
+        .write
+        .mode(SaveMode.Overwrite)
+        .option("header", "true")
+        .csv(out_dir + "year_" + year.toString + "_subscribers.csv")
+    })
   }
 
 
