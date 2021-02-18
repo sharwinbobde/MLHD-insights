f88546e71252aa6dc67b3651bcd63873ac89da93
Sharwin Bobde <bsharwin@gmail.com>
2021-02-18 10:57:12 +0100

    combined run.sh and run.conf.sh

--- scala-code/run.sh
+++ scala-code/run.sh
@@ -1,4 +1,15 @@
-source ./run.conf.sh
+#--------- Spark Configuration START --------------------------
+
+num_executors=1
+executor_cores=12
+executor_memory=20g
+driver_memory=3g
+jarfile='target/scala-2.12/MLHD-insights-assembly-1.0.jar'
+outdir='file:/io/data/processed/'
+#--------- Spark Configuration END ----------------------------
+
+
+
 HEIGHT=15
 WIDTH=40
 CHOICE_HEIGHT=4
